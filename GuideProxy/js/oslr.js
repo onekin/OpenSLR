a6ec9657bd1a949a038d0bd600c0b0635140af0e
Iker Azpeitia <iker.azpeitia@ehu.es>
2020-06-03 19:29:20 +0200

    Update oslr.js

--- GuideProxy/js/oslr.js
+++ GuideProxy/js/oslr.js
@@ -337,7 +337,7 @@ $( "#slides" ).load( url , function( response, status, xhr ) {
     }
 
   function connect() {
-      pub.cx = js2neo.open({
+      pub.cx = js2neo.open({secure:false,
           host: document.getElementById("host").value,
           port: parseInt(document.getElementById("port").value),
           user: document.getElementById("user").value,
